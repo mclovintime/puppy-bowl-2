d85b09e201bbe2efea4a9658aa92c909ed0daf35
Randy Pellegrin <rpell391@gmail.com>
2022-10-13 14:17:49 -0500

    css and images working

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,22 +1,26 @@
 import React from "react";
 
 const ListItem = (props) => {
-  { 
+  {
     return (
-        <div id="table">
-
-  {  props.players.length ? 
-      props.players.map((puppy) => {
-        //console.log(puppy, "is puppy output");
-          <div id="panel">
-            <div>{puppy.name}</div>
-            <div>{puppy}</div>
-          </div>
-      })    : <div>loading puppies...</div>}
-
-</div>
-        )
- 
-    }}
+      <div id="table">
+        {props.players.length ? (
+          props.players.map((puppy) => {
+            console.log(puppy, "is puppy output");
+            return (
+              <div id="panel">
+                <div>{puppy.name}</div>
+                <div>{puppy.id}</div>
+                <img class="puppyImage" src={puppy.imageUrl} />
+              </div>
+            );
+          })
+        ) : (
+          <div>loading puppies...</div>
+        )}
+      </div>
+    );
+  }
+};
 
 export default ListItem;
