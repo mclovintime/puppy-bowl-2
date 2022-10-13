6d59079517273e67528c7d1d08035275188d89d9
Randy Pellegrin <rpell391@gmail.com>
2022-10-13 10:55:16 -0500

    setup fetch function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Navbar} from './'
 
 const Main = () => {
+  useEffect(() => {
+    async function getPlayerData(){
+      try{
+        const getPlayers = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players'
+        );
+        const result = await getPlayers.json()
+        console.log(result)
+      } catch(err){
+        console.error(err)
+      }
+    }
+    getPlayerData()
+  }, [])
+
   return (
     <div id="main">
     <Navbar/>
