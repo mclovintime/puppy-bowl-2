3cc784a135b6dd5a068bdd28d6680ef10e0be079
Thomas McLaughlin <123tommygun1@gmail.com>
2022-10-14 11:22:56 -0400

    working on css

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,9 +9,9 @@ const ListItem = (props) => {
             console.log(puppy, "is puppy output");
             return (
               <div id="panel">
-                <div>{puppy.name}</div>
-                <div>{puppy.id}</div>
+                <div id="name"><b>{puppy.name}</b></div>
                 <img class="puppyImage" src={puppy.imageUrl} />
+                <div id="id"><i><b>{`#${puppy.id}`}</b></i></div>
               </div>
             );
           })
