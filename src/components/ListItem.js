b4be8d9c98fea073c822862c517bcbc58a719432
Randy Pellegrin <rpell391@gmail.com>
2022-10-14 14:28:45 -0500

    trying to pass signle puppy

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,18 +2,14 @@ import React from "react";
 import { Main } from ".";
 
 const ListItem = (props) => {
-  
   {
     return (
       <div id="table">
         {props.players.length ? (
           props.players.map((puppy) => {
-           
             return (
-              <div onClick={() =>  props.setSelectedPlayer(puppy.id)} id="panel">
-                
+              <div onClick={() => props.setSelectedPlayer(puppy.id)} id="panel">
                 <div id="name">
-                  
                   <b>{puppy.name}</b>
                 </div>
                 <img class="puppyImage" src={puppy.imageUrl} />
--- src/components/SinglePuppy.js
+++ src/components/SinglePuppy.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const SinglePuppy = ({puppyId, getPlayer}) => {
+const SinglePuppy = ({ puppyId, getPlayer }) => {
   console.log(getPlayer, "get player");
   console.log(puppyId, "selected player");
+  let placeHolder = getPlayer(puppyId);
+  console.log(placeHolder, "this is placeHolder");
   return (
     <div>
       <div>test test test</div>
